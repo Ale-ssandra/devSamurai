4ef6ba80cf0237ae7eaed520ceade454dd39d2a6
Ale-ssandra <lele.p.cunha@gmail.com>
2023-09-19 23:12:51 -0300

    raquetes

--- pingPong/script.js
+++ pingPong/script.js
@@ -1,6 +1,7 @@
 
       const canvas = document.getElementById("canvas");
       const ctx = canvas.getContext("2d");
+      const lineWidth = 15
 
       function setup() {
         canvas.width = window.innerWidth;
@@ -13,6 +14,23 @@
       function draw() {
         ctx.fillStyle = ("green");
         ctx.fillRect(0,0, window.innerWidth, window.innerHeight);
+
+// desenho da linha do campo
+        ctx.fillStyle = ("#fff")
+       const x = window.innerWidth / 2 - lineWidth / 2
+       const y = 0
+       const w = lineWidth
+       const h = window.innerHeight
+
+       ctx.fillRect(x, y , w , h)
+
+
+//desenhando raquete esquerda  
+        ctx.fillRect(10,400 , lineWidth , 200)
+
+
+ // desenhando raquete direita
+       ctx.fillRect(window.innerWidth - lineWidth - 10, 220, lineWidth, 200)        
       }
 
       setup()
